aa3442c52e2001db188cf328ec3c217eceed6c1c
weibin209 <weibindabest@gmail.com>
2018-10-03 07:54:07 +0800

    Update login page

--- client/src/pages/login.vue
+++ client/src/pages/login.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>asdfhahfklaeswhv</div>
+ <layout>
+   <div class="login">
+     <div class="login-box column">
+       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <h2>login</h2>
+         <div class="form-label-username">
+           <label>username:</label>
+           <input 
+           type="text" 
+           name="username"
+           placeholder="username">
+         </div>
+         
+        
+         <div class="form-label-username">
+           <label>password:</label>
+           <input 
+           type="text" 
+           name="password"
+           placeholder="password">
+         </div>
+         <button> sign</button>
+       </div>
+     </div>
+   </div>
+ </layout>
 </template>
 
 <script>

cd1200af676b25e0b29993a99d6d18429d21f3df
DavidTan0527 <david_weiucheng@hotmail.com>
2018-10-03 21:25:53 +0800

    Update student page

---
+++ client/src/pages/aid.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="_aid">
+    <layout>
+      <crudTable
+      title="助学金申请列表"
+      :columns="aidColumns"
+      :tableData="aidData"></crudTable>
+    </layout>
+  </div>
+</template>
+
+<script>
+import layout from '@/layout/default'
+import crudTable from '@/components/tables'
+import { aidColumns } from '../../api/tableColumns'
+export default {
+  components: {
+    layout,
+    crudTable
+  },
+  data() {
+    return {
+      aidColumns,
+      aidData: [
+        {
+          company_name: "我很想死集团",
+          finance_type: "赶紧死掉费用",
+          expired_date: "06092069",  
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/pages/student.vue
+++ client/src/pages/student.vue
@@ -3,8 +3,8 @@
     <layout>
       <crudTable
       title="学生列表"
-      :columns="societyColumns"
-      :tableData="data">
+      :columns="studentColumns"
+      :tableData="studentData">
       </crudTable>
     </layout>
   </div>
@@ -13,7 +13,7 @@
 <script>
 import crudTable from '@/components/tables'
 import layout    from '@/layout/default'
-import {studentColumns} from '../../api/tableColumns'
+import { studentColumns } from '../../api/tableColumns'
 export default {
   components: {
     crudTable,
@@ -21,12 +21,19 @@ export default {
   },
   data() {
     return {
-      studentColumns,
-      data : [{
-        cn_name: "陈某某",
-        class  : "搞搞丽丽",
-        aid_type: "吃到你爽",
-      }]
+      studentColumns, 
+      studentData : [
+        {
+          cn_name: "陈某某",
+          class  : "搞搞丽丽",
+          aid_type: "吃到你爽",
+        },
+        {
+          cn_name: "杀了我",
+          class  : "没死就好",
+          aid_type: "早日死掉",
+        }
+      ]
     }
   }
 }

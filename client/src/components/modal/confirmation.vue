54dad451045f5502d91cc50ef6657d1ddfe20b98
DavidTan0527 <david_weiucheng@hotmail.com>
2018-10-04 00:00:40 +0800

    Add addStudent page and modals

---
+++ client/src/components/modal/confirmation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="modal" id="editGroup" :class="{'active': active }">
+    <a @click="active = false" class="modal-overlay" aria-label="Close"></a>
+    <div class="modal-container">
+      <div class="modal-header">
+        <a @click="active = false" class="btn btn-clear float-right" aria-label="Close"></a>
+        <div class="modal-title h5">确认删除？</div>
+      </div>
+      <div class="modal-body">
+        删除后将无法恢复，请确认。
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" @click="active = false">取消</button>
+        <button class="btn btn-primary btn-error" @click="confirmClick">确认</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['confirm'],
+  data() {
+    return {
+      active: false,
+    }
+  },
+  methods: {
+    confirmClick() {
+      this.confirm()
+      this.active = false
+    }
+  }
+}
+</script>
---
+++ client/src/components/modal/modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal" id="editGroup" :class="{'active': active }">
+    <a @click="closeClick" class="modal-overlay" aria-label="Close"></a>
+    <div class="modal-container loading-lg" :class="{ 'loading' : loading }">
+      <div class="modal-header">
+        <a @click="closeClick" class="btn btn-clear float-right" aria-label="Close"></a>
+        <div class="modal-title h5">{{title}}</div>
+      </div>
+      <div class="modal-body">
+        <div class="toast toast-error" v-if="error">
+          {{ errorMessage }}
+        </div>
+        <slot name="content"></slot>
+      </div>
+      <div class="modal-footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+  },
+  data() {
+    return {
+      active : false,
+      loading: false,
+      error  : false,
+      errorMessage: "发生错误，请检查表单。"
+    }
+  },
+  methods: {
+    closeClick() {
+      this.active  = false
+      this.loading = false
+      this.error   = false
+      this.$emit('close')
+    }
+  }
+}
+</script>

5c6ece0940fed1e7843dc1bf70915fee7459bc76
DavidTan0527 <david_weiucheng@hotmail.com>
2018-10-05 00:09:38 +0800

    Fix steps bug

--- client/src/pages/addStudent.vue
+++ client/src/pages/addStudent.vue
@@ -8,11 +8,14 @@
         <el-step title="经济资料"></el-step>
       </el-steps>
 
-      <!-- <router-view :name="$route.name"></router-view> -->
+      <!-- form stuffs -->
       <basic v-if="$route.name === 'basic'"></basic>
       <parent v-if="$route.name === 'parent'"></parent>
+      
+      
+      <finish v-if="$route.name === 'finish'"></finish>
 
-      <div class="btn-group">
+      <div class="btn-group" v-if="active != 4">
         <el-button-group>
           <el-button type="primary" class="btnn" size="small" @click="prevStep">
             <i class="el-icon-arrow-left"></i> previous
@@ -23,6 +26,12 @@
         </el-button-group>
       </div>
 
+      <div class="finish-button" >
+        <button class="btn btn-primary btn-block" @click="finishStep">
+          <i class="icon icon-edit"></i> 完成
+        </button>
+      </div>
+
     </layout>
   </div>
 </template>
@@ -31,19 +40,30 @@
 import layout from '@/layout/default'
 import basic  from '@/pages/createStudent/studentform'
 import parent from '@/pages/createStudent/parent'
+import finish from '@/pages/createStudent/finish'
 export default {
   components: {
     layout,
     basic,
-    parent
+    parent,
+    finish
   },
   data() {
     return {
       active: 0,
+      medium: 0,
       id    : 0,
       paths : [
         'basic', 'parent', 'family', 'finance'
-      ]
+      ],
+    }
+  },
+  mounted() {
+    this.active = this.paths.indexOf(this.$route.name);
+    this.id = this.active;
+    if (this.active == -1 || this.id == -1) {
+      this.active = 4;
+      this.id = 4;
     }
   },
   methods: {
@@ -54,21 +74,57 @@ export default {
         this.id--;
       }
       const params = this.paths[this.id];
-      this.$router.push({ path: `/addStudent/${params}` })
+      this.$router.push({ path: `/addStudent/${params}` });
       this.active = this.paths.indexOf(this.$route.name);
     },
     nextStep() {
+      console.log("post")
       if (this.id == 3) {
-        this.id = 3;
+        this.id++;
+        this.active = 4;
+        const params = "finish";
+        this.$router.push({ path: `/addStudent/${params}` });
+        return;
         //finish upload
       } else {
         this.id++;
       }
       const params = this.paths[this.id];
-      this.$router.push({ path: `/addStudent/${params}` })
-      this.active = this.paths.indexOf(this.$route.name);
+      this.$router.push({ path: `/addStudent/${params}` });
+      this.$nextTick(function () {
+        this.active = this.paths.indexOf(this.$route.name);
+      })
     },
-  }
+    finishStep() {
+      console.log("finish");
+    }
+  },
+    // computed: {
+    //   nextStep: function() {
+    //     if (this.id == 3) {
+    //       this.id++;
+    //       const params = "finish";
+    //       this.$router.push({ path: `/addStudent/${params}` });
+    //       return;
+    //       //finish upload
+    //     } else {
+    //       this.id++;
+    //     }
+    //     const params = this.paths[this.id];
+    //     this.$router.push({ path: `/addStudent/${params}` });
+    //     this.active = this.paths.indexOf(this.$route.name);
+    //   },
+    //   prevStep: function() {
+    //     if (this.id == 0) {
+    //       this.id = 0;
+    //     } else {
+    //       this.id--;
+    //     }
+    //     const params = this.paths[this.id];
+    //     this.$router.push({ path: `/addStudent/${params}` });
+    //     this.active = this.paths.indexOf(this.$route.name);
+    //   }
+    // }
 }
 </script>
 

e5dc4a66f1b99efc13946f7a6cf444305c32b5c4
DavidTan0527 <david_weiucheng@hotmail.com>
2018-10-05 00:09:59 +0800

    Add forms finish page

---
+++ client/src/pages/createStudent/finish.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Show all details after all user input (return --- or error message if NULL or undefined)
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

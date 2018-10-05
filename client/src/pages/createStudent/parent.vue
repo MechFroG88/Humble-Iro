d611e922b299c0d7b9110244a5c5aefa1709ad40
DavidTan0527 <david_weiucheng@hotmail.com>
2018-10-05 22:34:46 +0800

    Add parent form

--- client/src/pages/createStudent/parent.vue
+++ client/src/pages/createStudent/parent.vue
@@ -10,13 +10,56 @@
 
     <form class="parent-form" v-for="num in quantity" :key="num">
       <h4>监护人{{num}}</h4>
-      <div class="form-group cn_name">
-        <label class="form-label" for="cn_name">中文姓名：</label>
-        <input class="form-input" type="text" id="cn_name" placeholder="中文姓名...">
+
+      <div class="form-container names columns">
+        <div class="form-group cn_name column col-6">
+          <label class="form-label" for="cn_name">中文姓名：</label>
+          <input class="form-input" type="text" id="cn_name" placeholder="中文姓名">
+        </div>
+        <div class="form-group en_name column col-6">
+          <label class="form-label" for="en_name">英文名字：</label>
+          <input class="form-input" type="text" id="en_name" placeholder="英文名字">
+        </div>
       </div>
-      <div class="form-group en_name">
-        <label class="form-label" for="en_name">英文名字：</label>
-        <input class="form-input" type="text" id="en_name" placeholder="英文名字...">
+
+      <div class="form-container personal-info columns">
+        <div class="form-group relationship column col-4">
+          <label class="form-label" for="relationship">关系：</label>
+          <select class="form-select" id="relationship">
+            <option>请选择关系</option>
+            <option>父亲</option>
+            <option>母亲</option>
+            <option>监护人</option>
+          </select>
+        </div>
+        <div class="form-group ic_num column col-5">
+          <label class="form-label" for="ic_num">身份证号码：</label>
+          <input class="form-input" type="text" id="ic_num" placeholder="身份证号码">
+        </div>
+        <div class="form-group age column col-3">
+          <label class="form-label" for="age">年龄：</label>
+          <input class="form-input" type="number" id="age" placeholder="年龄">
+        </div>
+      </div>
+
+      <div class="form-container work-info columns">
+        <div class="form-group occupation column col-4">
+          <label class="form-label" for="occupation">职业：</label>
+          <input class="form-input" type="text" id="occupation" placeholder="职业">
+        </div>
+        <div class="form-group occupation-name column col-4">
+          <label class="form-label" for="occupation-name">雇主/公司/工作单位名称：</label>
+          <input class="form-input" type="text" id="occupation-name" placeholder="雇主/公司/工作单位名称">
+        </div>
+        <div class="form-group head-phone column col-4">
+          <label class="form-label" for="head-phone">雇主联络号码：</label>
+          <input class="form-input" type="text" id="head-phone" placeholder="雇主联络号码">
+        </div>
+      </div>
+
+      <div class="form-group occupation-address">
+        <label class="form-label" for="occupation-address">工作地址</label>
+        <textarea class="form-input" id="occupation-address" placeholder="工作地址" rows="3"></textarea>
       </div>
     </form>
   </div>

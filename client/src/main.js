f55bf1c28b3a266790b610a9b3cb5c4042cad9e8
DavidTan0527 <david_weiucheng@hotmail.com>
2018-10-24 17:07:34 +0800

    Fix vee-validate and el-form conflict

--- client/src/main.js
+++ client/src/main.js
@@ -13,7 +13,24 @@ import './assets/element-ui.scss'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-Vue.use(VeeValidate);
+
+const config = {
+  aria: true,
+  classNames: {},
+  classes: false,
+  delay: 0,
+  dictionary: null,
+  errorBagName: 'errors', // change if property conflicts
+  events: 'input|blur',
+  fieldsBagName: 'v-fields',
+  i18n: null, // the vue-i18n plugin instance
+  i18nRootKey: 'validations', // the nested key under which the validation messages will be located
+  inject: true,
+  locale: 'en',
+  validity: false
+};
+
+Vue.use(VeeValidate, config);
 
 /* eslint-disable no-new */
 new Vue({

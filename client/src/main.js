6b23cb24f7f020e6b47ca967452f202e3d6347d9
DavidTan0527 <david_weiucheng@hotmail.com>
2018-12-20 23:37:28 +0800

    Add error message in basic form

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 import ElementUI from 'element-ui';
-import VeeValidate from 'vee-validate';
-import { getToken } from '@/utils/auth'
-import './assets/scss/icons/style.css'
-import './assets/scss/style.scss'
-import './assets/element-ui.scss'
+import VeeValidate, { Validator } from 'vee-validate';
+import { getToken } from '@/utils/auth';
+import './assets/scss/icons/style.css';
+import './assets/scss/style.scss';
+import './assets/element-ui.scss';
 
 Vue.config.productionTip = false
 
@@ -30,7 +30,53 @@ const config = {
   validity: false
 };
 
+const errorMessages = {
+  custom: {
+    cn_name: {
+      required: '请填上中文姓名'
+    },
+    en_name: {
+      required: '请填上英文姓名'
+    },
+    number: {
+      required: '请填上学号',
+      numeric: '学号只能含有数字',
+      length: '学号格式错误（XXXXXX）'
+    },
+    class: {
+      required: '请填上班级'
+    },
+    icNumber: {
+      required: '请填上身份证号码',
+      regex: () => '身份证格式错误（XXXXXX-XX-XXXX）'
+    },
+    phonenum: {
+      required: '请填上联络号码'
+    },
+    ancestral: {
+      required: '请填上祖籍'
+    },
+    birthdate: {
+      required: '请填上出生日期'
+    },
+    attitude: {
+      required: '请填上操行分数',
+      decimal: '操行分数必须为整数',
+      between: '分数只能介于0与100之间'
+    },
+    averageScore: {
+      required: '请填上总平均',
+      between: '分数只能介于0与100之间',
+      decimal: '总平均准确至3位小数'
+    },
+    address: {
+      required: '请填上住址'
+    }
+  }
+}
+
 Vue.use(VeeValidate, config);
+Validator.localize('en', errorMessages);
 
 /* eslint-disable no-new */
 new Vue({
--- client/src/pages/createStudent/basic.vue
+++ client/src/pages/createStudent/basic.vue
@@ -5,54 +5,70 @@
       <div class="form-group">
         <label class="form-label" for="cn_name">中文姓名</label>
         <input 
-          class="form-input" 
-          type="text" 
-          name="cn_name"
-          id="cn_name" 
-          placeholder="中文姓名"
-          v-validate="'required'"
-          v-model="value.cn_name">
+        class="form-input" 
+        :class="{'error': errors.has('cn_name')}"
+        type="text" 
+        name="cn_name"
+        id="cn_name" 
+        placeholder="中文姓名"
+        v-validate="'required'"
+        v-model="value.cn_name">
+        <div class="toast toast-error mt-1" v-if="errors.has('cn_name')">
+          {{errors.first('cn_name')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="en_name">英文姓名</label>
         <input 
-          class="form-input" 
-          type="text" 
-          id="en_name" 
-          name="en_name"
-          placeholder="英文姓名"
-          v-validate="{
-            required: true,
-            regex: /^[a-zA-Z ]+$/i
-          }"
-          v-model="value.en_name">
+        class="form-input" 
+        :class="{'error': errors.has('en_name')}"
+        type="text" 
+        id="en_name" 
+        name="en_name"
+        placeholder="英文姓名"
+        v-validate="{
+          required: true,
+          regex: /^[a-zA-Z ]+$/i
+        }"
+        v-model="value.en_name">
+        <div class="toast toast-error mt-1" v-if="errors.has('en_name')">
+          {{errors.first('en_name')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="number">学号</label>
         <input 
-          class="form-input" 
-          type="number" 
-          id="number" 
-          name="number"
-          v-validate="'required|length:6|integer'"
-          placeholder="学号"
-          v-model.number="value.number">
+        class="form-input" 
+        :class="{'error': errors.has('number')}"
+        type="text" 
+        id="number" 
+        name="number"
+        v-validate="'required|numeric|length:6'"
+        placeholder="学号"
+        v-model.number="value.number">
+        <div class="toast toast-error mt-1" v-if="errors.has('number')">
+          {{errors.first('number')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="class">班级</label>
         <input 
-          class="form-input" 
-          type="text" 
-          id="class" 
-          name="class"
-          v-validate="'required'"
-          placeholder="班级"
-          v-model="value.classroom">
+        class="form-input" 
+        :class="{'error': errors.has('class')}"
+        type="text" 
+        id="class" 
+        name="class"
+        v-validate="'required'"
+        placeholder="班级"
+        v-model="value.classroom">
+        <div class="toast toast-error mt-1" v-if="errors.has('class')">
+          {{errors.first('class')}}
+        </div>
       </div>
       <div class="form-group columns col-gapless">
         <label class="form-label col-12">性别</label>
         <label class="form-radio col-2">
-          <input type="radio" name="gender" checked
+          <input type="radio" name="gender"
           v-model="value.gender" :value="0">
           <i class="form-icon"></i> 男
         </label>
@@ -63,90 +79,116 @@
         </label>
       </div>
       <div class="form-group">  
-        <label class="form-label" for="ic-number">身份证号码</label>
+        <label class="form-label" for="icNumber">身份证号码</label>
         <input 
         class="form-input" 
+        :class="{'error': errors.has('icNumber')}"
         type="text" 
-        name="ic-number"
-        id="ic-number" 
+        name="icNumber"
+        id="icNumber" 
         v-validate="{
           required: true,
           regex: /^\d{6}-\d{2}-\d{4}$/
         }"
         placeholder="身份证号码"
         v-model="value.ic">
+        <div class="toast toast-error mt-1" v-if="errors.has('icNumber')">
+          {{errors.first('icNumber')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="phonenum">联络号码</label>
         <input 
-          class="form-input" 
-          type="text" 
-          name="phonenum"
-          v-validate="{
-            required: true,
-            regex: /^(\+6)?0(1(1|5)\d{8}|1[02-46-9]\d{7}|[2-79]\d{8}|8[0-9]\d{6})$/
-          }"
-          id="phonenum" 
-          placeholder="联络号码"
-          v-model="value.contact">
+        class="form-input" 
+        :class="{'error': errors.has('phonenum')}"
+        type="text" 
+        name="phonenum"
+        id="phonenum" 
+        v-validate="{
+          required: true,
+          regex: /^(\+6)?0(1\-?(1|5)\d{8}|1[02-46-9]\d{7}|[2-79]\d{8}|8[0-9]\d{6})$/
+        }"
+        placeholder="联络号码"
+        v-model="value.contact">
+        <div class="toast toast-error mt-1" v-if="errors.has('phonenum')">
+          {{errors.first('phonenum')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="ancestral">祖籍</label>
         <input 
-          class="form-input" 
-          type="text" 
-          id="ancestral" 
-          name="ancestral"
-          v-validate="'required'"
-          placeholder="祖籍"
-          v-model="value.ancestor">
+        class="form-input" 
+        :class="{'error': errors.has('ancestral')}"
+        type="text" 
+        id="ancestral" 
+        name="ancestral"
+        v-validate="'required'"
+        placeholder="祖籍"
+        v-model="value.ancestor">
+        <div class="toast toast-error mt-1" v-if="errors.has('ancestral')">
+          {{errors.first('ancestral')}}
+        </div>
       </div>
       <div class="form-group">
-        <label class="form-label" for="date-of-birth">出生日期</label>
+        <label class="form-label" for="birthdate">出生日期</label>
         <input 
-          class="form-input" 
-          type="date" 
-          id="date-of-birth" 
-          name="date-of-birth"
-          v-validate="{
-            required: true,
-          }"
-          placeholder="出生日期"
-          v-model="value.birthdate">
+        class="form-input" 
+        :class="{'error': errors.has('birthdate')}"
+        type="date" 
+        id="birthdate" 
+        name="birthdate"
+        v-validate="'required'"
+        placeholder="出生日期"
+        v-model="value.birthdate">
+        <div class="toast toast-error mt-1" v-if="errors.has('birthdate')">
+          {{errors.first('birthdate')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="attitude">操行</label>
         <input 
         class="form-input" 
+        :class="{'error': errors.has('attitude')}"
         type="text" 
         id="attitude" 
         name="attitude"
-        v-validate="'required|integer'"
+        v-validate="'required|decimal:0|between:0,100'"
         placeholder="操行"
         v-model.number="value.attitude">
+        <div class="toast toast-error mt-1" v-if="errors.has('attitude')">
+          {{errors.first('attitude')}}
+        </div>
       </div>
       <div class="form-group">
-        <label class="form-label" for="average-score">全年总平均</label>
+        <label class="form-label" for="averageScore">全年总平均</label>
         <input 
         class="form-input" 
+        :class="{'error': errors.has('averageScore')}"
         type="text" 
-        id="average-score" 
-        name="average-score"
-        v-validate="'required|decimal:3'"
+        id="averageScore" 
+        name="averageScore"
+        v-validate="'required|between:0,100|decimal:3'"
         placeholder="全年总平均"
         v-model.number="value.score">
+        <div class="toast toast-error mt-1" v-if="errors.has('averageScore')">
+          {{errors.first('averageScore')}}
+        </div>
       </div>
       <div class="form-group">
         <label class="form-label" for="address">住址</label>
         <textarea
         class="form-input" 
+        :class="{'error': errors.has('address')}"
         type="text" 
         id="address" 
         name="address"
         v-validate="'required'"
         rows="3"
         placeholder="住址"
-        v-model="value.address"></textarea>
+        v-model="value.address"/>
+        <div class="toast toast-error mt-1" v-if="errors.has('address')">
+          {{errors.first('address')}}
+        </div>
       </div>
     </form>
   </div>
@@ -169,7 +211,7 @@ export default {
         en_name: '',
         number: null,
         classroom: '',
-        gender: null,
+        gender: 0,
         ic: '',
         contact: null,
         ancestor: '',
